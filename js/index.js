02603c1f306f1f841921c989580e189c941464ea
Abigail Watanabe <abigail.watanabe@gmail.com>
2022-10-27 08:23:40 -0700

    cleans js

--- js/index.js
+++ js/index.js
@@ -42,39 +42,3 @@ document.getElementById("newTaskForm").addEventListener("submit", function() {
           newTaskVar.render();
       }
     });
-    
-    
-    //This is our original code see line 48 and below//
-    /*
-    document.getElementById("newTaskForm").addEventListener("submit", function() {
-    event.preventDefault();
-      var name = document.querySelector("#name").value;
-      var description = document.querySelector("#description").value;
-      var assignedTo = document.querySelector("#assignedTo").value;
-      var dueDate = document.querySelector("#dueDate").value;
-      var status = 'TODO';
-      if (name.length === 0 || description.length === 0 || assignedTo.length === 0 || dueDate.length === 0){
-      console.log('Please fill out all fields!');
-        var myAlert = document.getElementById('alertMe');
-        myAlert.style.display = 'block';
-      } else {
-      console.log('All fields filled!');
-        var myAlert = document.getElementById('alertMe');
-        myAlert.style.display = 'none';
-        newTaskVar.addTask();
-        newTaskVar.render();  
-      } 
-    });
-    
-    var tasksList = document.querySelector("#taskList");
-    
-    tasksList.addEventListener('click', (event) => {
-        if (event.target.classList.contains('done-button')) {
-            const parentTask = event.target.parentElement;
-            const taskId = Number(parentTask.dataset.taskId);
-            const task = newTaskVar.getTaskById(taskId);
-            task.status = 'DONE';
-            newTaskVar.render();
-        }
-    });
-    */
